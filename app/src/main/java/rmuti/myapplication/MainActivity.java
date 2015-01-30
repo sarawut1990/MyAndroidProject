2440984f35776896a0acd0741a505656b672336e
sarawut1990 <sarawutandaim@gmail.com>
2015-01-30 12:48:27 +0700

    Work1

--- app/src/main/java/rmuti/myapplication/MainActivity.java
+++ app/src/main/java/rmuti/myapplication/MainActivity.java
@@ -2,8 +2,12 @@ package rmuti.myapplication;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,7 +16,29 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button but = (Button)findViewById(R.id.button);
+        final EditText userName = (EditText)findViewById(R.id.username);
+        final EditText passWord = (EditText)findViewById(R.id.passWord);
+        but.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String user = userName.getText().toString();
+                String pass = passWord.getText().toString();
+                if(user.equals(pass)){
+                    Log.v("test", "same value");
+                }
+                else {
+                    Log.v("test", "difference value");
+                }
+            }
+        });
     }
+
+    //@Override
+    //protected void onCreate(Bundle savedInstanceState) {
+        //super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+    //}
 
 
     @Override
